1ea6fad417ea2ce074590aa5e0dbc81a5c23671d
Dmytro Ostryi(asdasd-mjeesh) <201300@nuos.edu.ua>
2022-11-07 10:16:55 +0200

    validator update

--- src/main/java/com/gfl/resources_server/service/proxy/ProxyValidatorService.java
+++ src/main/java/com/gfl/resources_server/service/proxy/ProxyValidatorService.java
@@ -1,16 +1,19 @@
 package com.gfl.resources_server.service.proxy;
 
 import com.gfl.resources_server.model.ProxyConfigHolder;
+import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Proxy;
 import java.net.SocketAddress;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ProxyValidatorService implements ProxyValidator {
@@ -24,9 +27,11 @@ public class ProxyValidatorService implements ProxyValidator {
             SocketAddress socketAddress = new InetSocketAddress(
                     proxyConfigHolder.getProxyNetworkConfig().getHostname(),
                     proxyConfigHolder.getProxyNetworkConfig().getPort());
+
             Proxy proxy = new Proxy(Proxy.Type.HTTP, socketAddress);
 
             OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
+                    .connectTimeout(60L, TimeUnit.SECONDS)
                     .proxy(proxy)
                     .build();
 
@@ -35,17 +40,15 @@ public class ProxyValidatorService implements ProxyValidator {
                     .url("http://info.cern.ch/")
                     .build();
 
-            var call = okHttpClient.newCall(request);
+            Call call = okHttpClient.newCall(request);
             Response httpResponse = call.execute();
-            System.out.println(httpResponse.code());
 
-            // TODO: 04.11.2022
-            //  we need figure out how to increase request timeOut to fix that exception.
-            //  and then just return true/false in depends of http response code.
+            System.out.println(httpResponse.code()); // TODO: 07.11.2022 change it to logging, please
 
+            return httpResponse.code() == HttpStatus.OK.value();
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage()); // TODO: 07.11.2022 change it to logging, please
+            return false;
         }
-        return true;
     }
 }
